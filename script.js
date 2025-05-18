fee79b378044b17b71899dfcc9e5296f98fb0373
angelapacheco29 <58189439+angelapacheco29@users.noreply.github.com>
2025-05-18 02:10:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+  window.onload = function() {
+        document.getElementById("Botao").addEventListener("click", function() {
+          document.getElementById("mensagem").innerText = "Você é demais!";
+        });
+      };
